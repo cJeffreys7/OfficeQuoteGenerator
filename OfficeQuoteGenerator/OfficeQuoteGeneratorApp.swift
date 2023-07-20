2e60f3d79b4012ef140700bb4b25971aa85d805c
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2023-07-20 09:13:32 -0500

    Initial Commit

---
+++ OfficeQuoteGenerator/OfficeQuoteGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfficeQuoteGeneratorApp.swift
+//  OfficeQuoteGenerator
+//
+//  Created by Chris Jeffreys on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct OfficeQuoteGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
